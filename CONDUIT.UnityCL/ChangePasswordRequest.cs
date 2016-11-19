03dead15f1113d29f745199bb6e3804517d7ca26
ShawnFreeman-PC <shawn.freman@gmail.com>
2016-11-18 22:37:28 -0500

    Correcting project naming fuck up

--- CONDUIT.UnityCL/ChangePasswordRequest.cs
+++ CONDUIT.UnityCL/ChangePasswordRequest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace UnityCL
+namespace CONDUIT.UnityCL
 {
     [Serializable]
     public class ChangePasswordRequest
