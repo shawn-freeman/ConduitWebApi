0d349d65b8c0ff264c56be29e187939c3a2e2a06
ShawnFreeman-PC <shawn.freman@gmail.com>
2016-11-18 22:22:08 -0500

    Initial Unity Class Library checkin with change password transport class

---
+++ UnityCL/ChangePasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCL
+{
+    [Serializable]
+    public class ChangePasswordRequest
+    {
+        public int UserId { get; set; }
+        public int CurrentPassword { get; set; }
+        public int NewPassword { get; set; }
+    }
+}
